73fd3345ae9b4ed93944db8d47347bf16bcaa32c
Mark Tetteh <128140368+mark-tetteh@users.noreply.github.com>
2023-03-29 14:35:18 +0000

    Update Main.java

--- src/o_c/Main.java
+++ src/o_c/Main.java
@@ -6,18 +6,15 @@ import o_c.objects_classes.Trader1;
 public class Main {
     public static void main(String[] args) {
 
-//        System.out.print("Please enter your age :");
-//        Scanner scanner = new Scanner(System.in);
-//        int input = scanner.nextInt();
-//        System.out.println(input + " 8is your age");
+
           Trade1 t1 = new Trade1("T1", "APPL", 100, 15.25);
           Trade1 t2 = new Trade1("T1", "APPL", 10, 15.0);
-//
+
           Trader1 T1 = new Trader1();
-//
+
             T1.addTrade(t1);
             T1.addTrade(t2);
-//
+
           T1.displayAccount();
 
         }
